a1d1492d081060795a75c9bc72576609bbc53574
huangxin <446768454@qq.com>
2019-05-24 14:29:48 +0800

    补提交部分礼包店UI

---
+++ app/src/main/java/com/zthx/npj/ui/MembershipPackageActivity.java
@@ -0,0 +1,74 @@
+package com.zthx.npj.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.zthx.npj.R;
+import com.zthx.npj.adapter.DiscoverViewPagerAdapter;
+import com.zthx.npj.ui.fragment.BuyGiftFragment;
+import com.zthx.npj.view.MyCircleView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MembershipPackageActivity extends AppCompatActivity {
+
+    @BindView(R.id.at_location_store_tv_ruzhu)
+    TextView atLocationStoreTvRuzhu;
+    @BindView(R.id.title)
+    RelativeLayout title;
+    @BindView(R.id.at_membership_package_head_pic)
+    MyCircleView atMembershipPackageHeadPic;
+    @BindView(R.id.at_membership_package_tv_name)
+    TextView atMembershipPackageTvName;
+    @BindView(R.id.at_membership_package_tv_tuijian)
+    TextView atMembershipPackageTvTuijian;
+    @BindView(R.id.at_membership_package_tv_share)
+    TextView atMembershipPackageTvShare;
+    @BindView(R.id.at_membership_package_tv_logo)
+    TextView atMembershipPackageTvLogo;
+    @BindView(R.id.at_membership_package_tb)
+    TabLayout atMembershipPackageTb;
+    @BindView(R.id.at_membership_package_viewpager)
+    ViewPager atMembershipPackageVp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_membership_package);
+        ButterKnife.bind(this);
+
+        List<String> list = new ArrayList<>();
+        list.add("购买礼包");
+        list.add("代言人权益");
+        List<Fragment> list2 = new ArrayList<>();
+        list2.add(new BuyGiftFragment());
+        list2.add(new BuyGiftFragment());
+        DiscoverViewPagerAdapter mAdapter = new DiscoverViewPagerAdapter(getSupportFragmentManager(), this, list, list2);
+        atMembershipPackageVp.setAdapter(mAdapter);
+        atMembershipPackageTb.setTabMode(TabLayout.MODE_FIXED);
+        atMembershipPackageTb.setTabGravity(TabLayout.GRAVITY_CENTER);
+        atMembershipPackageTb.setupWithViewPager(atMembershipPackageVp);
+    }
+
+    @OnClick({R.id.at_membership_package_tv_tuijian, R.id.at_membership_package_tv_share})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.at_membership_package_tv_tuijian:
+                break;
+            case R.id.at_membership_package_tv_share:
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/zthx/npj/ui/fragment/BuyGiftFragment.java
@@ -0,0 +1,80 @@
+package com.zthx.npj.ui.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zthx.npj.R;
+import com.zthx.npj.adapter.BuyGiftAdapter;
+import com.zthx.npj.adapter.OrderListAdapter;
+import com.zthx.npj.net.been.CommentGoodsBeen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * create an instance of this fragment.
+ */
+public class BuyGiftFragment extends Fragment {
+
+
+    @BindView(R.id.fg_buy_gift_rv)
+    RecyclerView fgBuyGiftRv;
+    Unbinder unbinder;
+
+    public BuyGiftFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_buy_gift, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        LinearLayoutManager manager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
+        fgBuyGiftRv.setLayoutManager(manager);
+        List<CommentGoodsBeen> list = new ArrayList<>();
+        list.add(new CommentGoodsBeen());
+        list.add(new CommentGoodsBeen());
+        list.add(new CommentGoodsBeen());
+        list.add(new CommentGoodsBeen());
+        list.add(new CommentGoodsBeen());
+        list.add(new CommentGoodsBeen());
+        BuyGiftAdapter mAdapter = new BuyGiftAdapter(getActivity(), list);
+        fgBuyGiftRv.setAdapter(mAdapter);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
